d9d4a7581dab4ea52e41d61cba9d43f08e559567
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-05 18:11:10 -0700

    updated to do list, starting to add responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import Cards from './components/Cards'
+
+const style ={
+  card: {
+    padding: '5vw'
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
+      <div className='page-body'>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      <Cards style={style.card}/>
+      </div>
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+const style = {
+    nav: {
+        width: '100vw',
+    }
+}
+
+class NavBar extends Component {
+state = { activeItem: 'home' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+    <div class="ui top fixed menu">
+        <Segment inverted style={style.nav}>
+            <Menu inverted pointing secondary>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item
+                name='messages'
+                active={activeItem === 'messages'}
+                onClick={this.handleItemClick}
+            />
+            <Menu.Item
+                name='friends'
+                active={activeItem === 'friends'}
+                onClick={this.handleItemClick}
+            />
+            </Menu>
+        </Segment>
+    </div>
+        )
+    }
+}
+export default NavBar
