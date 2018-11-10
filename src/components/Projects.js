90e86dfb4046560fc969b3c00caa70577dda2871
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-09 21:34:54 -0700

    moved technologies and projects info to .json files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Card, Icon, Image, Reveal } from 'semantic-ui-react'
-
-const beers = {creation: 'October 2018', img: '/img/Beers.png', name: 'Full CRUD Beer Review App', url: 'https://warm-garden-50264.herokuapp.com/', gitHub: 'https://github.com/Kyle-Ski/College-database-Front-End', description: 'A full CRUD front end React app that uses Semantic-Ui. It connects to a back end PostgreSQL database and Knex RESTful server. Technologies: Semantic-Ui React, React, PostgreSQL, Knex.js.'}
-const bookStore = {creation: 'September 2018', img: '/img/Bookstore.png', name: 'React Bookstore', url: 'http://greedy-drink.surge.sh/', gitHub: 'https://github.com/Kyle-Ski/Bookstore', description: 'My first React App. Created to work like a shopping cart for a bookstore with search functionality. Technologies: React, CSS, JavaScript, HTML'}
-const expressServer = {creation: 'October 2018', img: '/img/ExpressServer.png', name: 'RESTful Express Server', url: 'https://protected-headland-83497.herokuapp.com/', gitHub: 'https://github.com/svphan1/express-server-mastery', description: 'An Express server created using RESTful programming, MCV architecture, and pair programming. Technologies: Express.js, JavaScript'}
-const inbox = {creation: 'September 2018', img: '/img/Inbox.png', name: 'React Inbox', url: 'https://github.com/Kyle-Ski/react-inbox', gitHub: 'https://github.com/Kyle-Ski/react-inbox', description: 'A React app created to look like an email inbox. This app was my first attempt at making patch, post and delete requests to an API. Technologies: Bootstrap, JavaScript, React, HTML, CSS'}
-const pixle = {creation: 'August 2018', img: '/img/PixleArt.png', name: 'Pixle Art Maker', url: 'http://lonely-cover.surge.sh/', gitHub: 'https://github.com/Kyle-Ski/pixel-art-maker', description: 'My introduction to DOM manipulation and Event Listeners. Technologies: JavaScript, HTML, and CSS.'}
-
-const projects = [beers, expressServer, inbox, bookStore, pixle]
+import projects from '../data/projectsData'
 
 const style = {
     image: {
         minHeight: '150px'
     }
 }
+
 const Projects = () => (
     projects.map((project ,i) => {
         return(
--- src/components/TechCards.js
+++ src/components/TechCards.js
@@ -1,26 +1,6 @@
 import React from 'react'
-import { Label, Image} from 'semantic-ui-react'
-const bootstrap = {name : 'Bootstrap 3 & 4', img: '/img/Bootstrap-Logo.png', icon: 'tv', description: 'Front-End'}
-const css = {name: 'CSS3', img: '/img/CSS3-Logo.png', icon: 'css3 alternate', description: 'Front-End'}
-const git = {name: 'Git', img: '/img/Git-Logo.png', icon: 'github square', description: 'Front/Back-End'}
-const heroku = {name: 'Heroku', img: '/img/Heroku-Logo.png', icon: 'tv', description: 'Back-End'}
-const html = {name: 'HTML5', img: '/img/HTML5-Logo.png', icon: 'html5', description: 'Front-End'}
-const javaScript= {name: 'JavaScript', img: '/img/JavaScript-Logo.jpg', icon: 'js square', description: 'Front/back'}
-const knexJs = {name: 'Knex.js', img: '/img/KnexJs-Logo.png', icon: 'server', description: 'Back-End'}
-const nodeJs = {name: 'Node.js', img: '/img/NodeJs-Logo.png', icon: 'server', description: 'Back-End'}
-const postgre = {name: 'PostgreSQL', img: '/img/PostgreSQL-Logo.png', icon: 'server', description: 'Back-End'}
-const reactL = {name: 'React', img: '/img/React-Logo.png', icon: 'react', description: 'Front-End'}
-const rest = {name: 'RESTful API', img: '/img/RestAPI-Logo.png', icon: 'server', description: 'Back-End'}
-const logos = [bootstrap, css, git, heroku, html, javaScript, knexJs, nodeJs, postgre, reactL, rest]
-
-const style = {
-    img: {
-        backgroundColor: 'white'
-    }, 
-    extra: {
-        borderColor: 'white'
-    }
-}
+import { Image } from 'semantic-ui-react'
+import logos from '../data/techData'
 
 const TechCards = () => (
     logos.map((logo, i) => {
