b6e960f9bfe32dd5412ccc9ed75230c9bca696d2
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-01-18 19:26:42 -0700

    changed resume

--- src/components/ParallaxContent.js
+++ src/components/ParallaxContent.js
@@ -117,10 +117,10 @@ const ParallaxTest = () => (
             </Parallax>
             <Divider />
             <Responsive minWidth={768}>
-                <iframe title='desktop' id='resume' src="https://docs.google.com/viewer?url=https://raw.githubusercontent.com/Kyle-Ski/Personal-Site2/6102301023e871e828b9b096fb8932a5b373fa5c/public/img/Resume_almostFinal.pdf&embedded=true" height="700px" width="75%"></iframe>
+                <iframe title='desktop' id='resume' src="https://docs.google.com/viewer?url=https://raw.githubusercontent.com/Kyle-Ski/Personal-Site2/8ef4e072149e95d8ea21ccd1df6e98c8cf8f6976/src/Resume.pdf&embedded=true" height="700px" width="75%"></iframe>
             </Responsive>
             <Responsive maxWidth={767}>
-                <iframe title='smaller' id='resume' src="https://docs.google.com/viewer?url=https://raw.githubusercontent.com/Kyle-Ski/Personal-Site2/6102301023e871e828b9b096fb8932a5b373fa5c/public/img/Resume_almostFinal.pdf&embedded=true" height="500px" width="100%"></iframe>
+                <iframe title='smaller' id='resume' src="https://docs.google.com/viewer?url=https://raw.githubusercontent.com/Kyle-Ski/Personal-Site2/8ef4e072149e95d8ea21ccd1df6e98c8cf8f6976/src/Resume.pdf&embedded=true" height="500px" width="100%"></iframe>
             </Responsive>
             <Divider />
             <List id='contact' style={style.list} horizontal>
