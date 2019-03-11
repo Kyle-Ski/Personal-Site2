9b38512c2b0983f7f97a736e05a2ecf8ec789303
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-03-11 16:11:03 -0600

    fixing projects, updating resume..

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-import projects from '../data/projectsData'
+import React from "react"
+import { Card, Icon, Image } from "semantic-ui-react"
+import projects from "../data/projectsData"
 
 const style = {
-    image: {
-        minHeight: '150px'
-    }
+  image: {
+    minHeight: "150px"
+  }
 }
 
-const Projects = () => (
-    projects.map((project ,i) => {
-        return(
-            <Card className='hover' key={i}>
-                <Image src={project.img} style={style.image}/>
-                <Card.Content>
-                    <Card.Header>{project.name}</Card.Header>
-                    <Card.Meta>{project.creation}</Card.Meta>
-                    <Card.Description>{project.description}</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <a href={project.url} rel="noopener noreferrer" target='_blank'>
-                        <Icon name='world' size='large'/>
-                        Live Url
-                    </a>
-                </Card.Content>
-                <Card.Content extra>
-                    <a href={project.gitHub} rel="noopener noreferrer" target='_blank'>
-                        <Icon name='github' size='large'/>
-                        GitHub Repo
-                    </a>        
-                </Card.Content>
-            </Card>
-        )
-    })
-)
+const Projects = () =>
+  projects.map((project, i) => {
+    return (
+      <Card className="hover" key={i}>
+        <Image src={project.img} style={style.image} />
+        <Card.Content>
+          <Card.Header>{project.name}</Card.Header>
+          <Card.Meta>{project.creation}</Card.Meta>
+          <Card.Description>{project.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          {project.url.length ? (
+            <a href={project.url} rel="noopener noreferrer" target="_blank">
+              <Icon name="world" size="large" />
+              Live Url
+            </a>
+          ) : (
+            "Live URL Comming Soon! Check the GitHub for instructions."
+          )}
+        </Card.Content>
+        <Card.Content extra>
+          <a href={project.gitHub} rel="noopener noreferrer" target="_blank">
+            <Icon name="github" size="large" />
+            GitHub Repo
+          </a>
+        </Card.Content>
+      </Card>
+    )
+  })
 
 export default Projects
